7c2be444ddad6ec5b9936107f3c79f4c0f66e72c
mahrad <mahradti@gmail.com>
2024-04-03 13:50:11 +0330

    The Footer has been created

--- education-website/src/components/container/Footer.jsx
+++ education-website/src/components/container/Footer.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
+import {BsInstagram , BsTwitter , BsPinterest , BsFacebook} from "react-icons/bs"
+import {motion} from "framer-motion"
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <motion.div
+        initial={{height: 0}}
+        whileInView={{height: "auto"}}
+        transition={{duration: 1}}
+        className='bg-Teal p-10'
+        >
+            <div className='grid md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 place-items-start gap-8 text-white'>
+                <div>
+                    <div className='font-bold mb-6'>
+                        Get Started
+                    </div>
+                    <p className='text-sm leading-7'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident unde incidunt eveniet repellat voluptatibus temporibus delectus voluptatum? Quaerat.</p>
+                </div>
+                <div>
+                    <div className='font-bold mb-6'>
+                        Services
+                    </div>
+                    <div className='flex flex-col gap-4'>
+                        <a href="" className='text-sm hover:underline'>Web Design</a>
+                        <a href="" className='text-sm hover:underline'>Web Development</a>
+                        <a href="" className='text-sm hover:underline'>Science</a>
+                        <a href="" className='text-sm hover:underline'>Digital Marketing</a>
+                    </div>
+                </div>
+                <div>
+                    <div className='font-bold mb-6'>Company</div>
+                    <div className='flex flex-col gap-4'>
+                        <a href="" className='text-sm hover:underline'>Privacy Policy</a>
+                        <a href="" className='text-sm hover:underline'>Sitemap</a>
+                        <a href="" className='text-sm hover:underline'>Careers</a>
+                        <a href="" className='text-sm hover:underline'>Term & Conditions</a>
+                    </div>
+                </div>
+                <div>
+                    <div className='font-bold mb-6'>Follow Us</div>
+                    <div className='text-sm mb-4'>
+                       Mahradti@gmail.com
+                    </div>
+                    <div className='text-sm'>+989356902177</div>
+                    <div className='flex gap-4 mt-4'>
+                        <a href="" className='hover:scale-110 text-xl'><BsFacebook/></a>
+                        <a href="" className='hover:scale-110 text-xl'><BsInstagram/></a>
+                        <a href="" className='hover:scale-110 text-xl'><BsTwitter/></a>
+                        <a href="" className='hover:scale-110 text-xl'><BsPinterest/></a>
+                    </div>
+                </div>
+
+            </div>
+        </motion.div>
     );
 }
 
