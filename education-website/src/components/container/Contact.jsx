d58a8ca6d32ac12e984b5de4fb966aed20240068
mahrad <mahradti@gmail.com>
2024-04-03 12:26:43 +0330

    The Contact has been created

--- education-website/src/components/container/Contact.jsx
+++ education-website/src/components/container/Contact.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import {motion} from "framer-motion"
 const Contact = () => {
     return (
         <div className='section' id='contact'>
-            Contact
+            <div className='text-center max-w-[600px] mx-auto'>
+                <div className='sm:text-3xl text-2xl font-bold mb-5'>Subscribe Newsletter</div>
+                <p className='text-sm leading-7 text-gray'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus magnam veritatis quae et deleniti, aspernatur odio neque iusto tempore sapiente? lorem10 Lorem ipsum dolor sit amet.</p>
+                <motion.form 
+                initial={{scale: 0}}
+                whileInView={{scale: 1}}
+                transition={{duration: 0.3}}
+                className='mt-5'>
+                    <input type="text" placeholder='Enter your Email' className='sm:p-3 p-2 outline-none text-sm shadow-md sm:w-72 w-60'/>
+                    <button className='text-sm text-white bg-Teal sm:p-3 p-2 font-bold'>Subscribe</button>
+                </motion.form>
+            </div>
         </div>
     );
 }
